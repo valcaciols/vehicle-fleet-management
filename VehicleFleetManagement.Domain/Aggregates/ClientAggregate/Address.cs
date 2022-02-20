2ffab2ec90967a1bc0173eae64a89ed4e14e0a8a
Leonardo Valcacio <ls.valcacio@gmail.com.br>
2022-02-20 12:20:27 -0400

    feat: adicionando clientAggregate

---
+++ VehicleFleetManagement.Domain/Aggregates/ClientAggregate/Address.cs
@@ -0,0 +1,25 @@
+﻿using VehicleFleetManagement.Domain.SeedWork;
+
+namespace VehicleFleetManagement.Domain.Aggregates.ClientAggregate
+{
+    public class Address: Entity
+    {
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public int Cep { get; private set; }
+
+        public Address(string street, string city, int cep)
+        {
+            Street = street;
+            City = city;
+            Cep = cep;
+        }
+
+        public void Change(string street, string city, int cep)
+        {
+            Street = Street.Equals(street) ? Street : street;
+            City = Street.Equals(city) ? City : city;
+            Cep = Cep.Equals(cep) ? Cep : cep;
+        }        
+    }
+}
---
+++ VehicleFleetManagement.Domain/Aggregates/ClientAggregate/Client.cs
@@ -0,0 +1,27 @@
+﻿using VehicleFleetManagement.Domain.SeedWork;
+
+namespace VehicleFleetManagement.Domain.Aggregates.ClientAggregate
+{
+    public class Client: Entity, IAggregateRoot
+    {
+        public string Name { get; private set; }
+        public int Cpf { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public int Cnh { get; private set; }
+        public Address Address { get; private set; }
+
+        public Client(string name, int cpf, DateTime birthDate, int cnh, Address address)
+        {
+            Name = name;
+            Cpf = cpf;
+            BirthDate = birthDate;
+            Cnh = cnh;
+            Address = address;
+        }
+
+        public void ChangeAddress(string street, string city, int cep)
+        {
+            Address.Change(street, city, cep);
+        }
+    }
+}
