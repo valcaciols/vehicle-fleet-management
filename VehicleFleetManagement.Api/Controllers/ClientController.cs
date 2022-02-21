50b48c4a36755a33616de27773fb7675186e6c01
Leonardo Valcacio <ls.valcacio@gmail.com.br>
2022-02-21 06:10:06 -0400

    feat: adicionando controlers

---
+++ VehicleFleetManagement.Api/Controllers/ClientController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using VehicleFleetManagement.Application.ViewModels.Client;
+using VehicleFleetManagement.Application.ViewModels.Requests;
+using VehicleFleetManagement.Domain.Aggregates.ClientAggregate;
+using VehicleFleetManagement.Application.Commands;
+using VehicleFleetManagement.Application.CommandHandlers.Clients;
+using VehicleFleetManagement.Application.ViewModels.Responses;
+using MediatR;
+
+namespace VehicleFleetManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ClientController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        //[HttpGet]
+        //[ProducesResponseType(typeof(List<ClientViewModel>), StatusCodes.Status200OK)]
+        //public IActionResult GetAll([FromQuery] ClientRequest clientRequest)
+        //{
+        //    return Ok();
+        //}
+
+        //[HttpGet("{id}")]
+        //[ProducesResponseType(typeof(ClientViewModel), StatusCodes.Status200OK)]
+        //public IActionResult Get(int id)
+        //{
+        //    return Ok();
+        //}
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ClientResponse),StatusCodes.Status200OK)]
+        public async Task<IActionResult> Post([FromBody] CreateClientCommand command)
+        {
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+    }
+}
--- VehicleFleetManagement.Api/Program.cs
+++ VehicleFleetManagement.Api/Program.cs
@@ -1,3 +1,9 @@
+using MediatR;
+using System.Reflection;
+using VehicleFleetManagement.Application.Commands;
+using VehicleFleetManagement.Domain.Aggregates.ClientAggregate;
+using VehicleFleetManagement.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMediatR(typeof(CreateClientCommand));
+
+builder.Services.AddTransient<IClientRepository, ClientRepository>();
+builder.Services.AddTransient<IAddressRepository, AddressRepository>();
 
 var app = builder.Build();
 
