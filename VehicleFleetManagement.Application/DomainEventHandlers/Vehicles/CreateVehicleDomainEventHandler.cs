eeb55f1071b4dbab04b01f88df80626867af1912
Leonardo Valcacio <ls.valcacio@gmail.com.br>
2022-02-21 23:52:10 -0400

    feat: adicionando evento de dominio vehicle

--- VehicleFleetManagement.Application/DomainEventHandlers/Vehicles/CreateVehicleDomainEventHandler.cs
+++ VehicleFleetManagement.Application/DomainEventHandlers/Vehicles/CreateVehicleDomainEventHandler.cs
@@ -1,14 +1,32 @@
 ﻿using MediatR;
 using VehicleFleetManagement.Application.DomainEvents.Clients;
+using VehicleFleetManagement.Domain.Denormalizeds;
+using VehicleFleetManagement.Domain.Denormalizeds.Repositories;
 
 namespace VehicleFleetManagement.Application.DomainEventHandlers.Clients
 {
     public class CreateVehicleDomainEventHandler : INotificationHandler<CreateVehicleDomainEvent>
     {
+        private readonly IDenormalizedVehicleRepository _denormalizedVehicle;
+
+        public CreateVehicleDomainEventHandler(IDenormalizedVehicleRepository denormalizedVehicle)
+        {
+            _denormalizedVehicle = denormalizedVehicle;
+        }
+
         public async Task Handle(CreateVehicleDomainEvent notification, CancellationToken cancellationToken)
         {
 
-            await Task.CompletedTask;
+            var vehicle = new DenormalizedVehicle
+            {
+                VehicleId = notification.VehicleId,
+                LicensePlate = notification.LicensePlate,
+                ModelManufacturer = notification.ModelManufacturer,
+                ModelName = notification.ModelName
+            };
+
+            await _denormalizedVehicle.AddAsync(vehicle);
+
         }
     }
 }
